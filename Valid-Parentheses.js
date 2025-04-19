3e0707c47524c8a2dab16109ef8cbd371945871f
Varsha <varshhhh6@gmail.com>
2025-04-19 20:16:20 +0530

    [20] [Time Beats: 76.26%] [Memory Beats: 83.51%] - LeetPush

---
+++ Valid-Parentheses.js
@@ -0,0 +1,19 @@
+var isValid = function(s) {
+    let stack = [];
+    let map = {
+        ')': '(', 
+        ']': '[', 
+        '}': '{'
+    };
+
+    for (let char of s) {
+        if (char in map) {
+            let top = stack.length === 0 ? '#' : stack.pop();
+            if (top !== map[char]) return false;
+        } else {
+            stack.push(char);
+        }
+    }
+
+    return stack.length === 0;
+};
