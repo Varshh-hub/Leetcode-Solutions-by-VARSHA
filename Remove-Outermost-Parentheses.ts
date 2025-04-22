277169f4836b305958babc7490723955dff77e2c
Varsha <varshhhh6@gmail.com>
2025-04-22 19:07:28 +0530

    [1021] [Time Beats: 84.27%] [Memory Beats: 86.52%] - LeetPush

---
+++ Remove-Outermost-Parentheses.ts
@@ -0,0 +1,27 @@
+var removeOuterParentheses = function(s) 
+{
+    let res = "";
+    let count = 0;
+
+    for (let i = 0; i < s.length; i++) 
+    {
+        if (s[i] === '(') 
+        {
+            if (count > 0) 
+            {
+                res += s[i];  
+            }
+            count++;
+        } 
+        else 
+        {
+            count--;
+            if (count > 0) 
+            {
+                res += s[i];  
+            }
+        }
+    }
+
+    return res;
+};
