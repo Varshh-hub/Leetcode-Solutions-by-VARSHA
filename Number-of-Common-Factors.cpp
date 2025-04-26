537600a0daa95c3f0dbf0f5e1bd2f99107a20f23
Varsha <varshhhh6@gmail.com>
2025-04-26 20:17:55 +0530

    [2427] [Time Beats: 100.00%] [Memory Beats: 82.83%] - LeetPush

---
+++ Number-of-Common-Factors.cpp
@@ -0,0 +1,18 @@
+class Solution 
+{
+public:
+    int commonFactors(int a, int b) 
+    {
+    int minVal = a < b ? a : b;
+    int count = 0;
+    for (int i = 1; i <= minVal; ++i) 
+    {
+        if (a % i == 0 && b % i == 0) 
+        {
+            ++count;
+        }
+    }
+
+    return count;
+    }
+};
