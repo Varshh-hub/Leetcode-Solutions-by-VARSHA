eb20749c328684e733f62d2d8aed07b8de2b8da2
Varsha <varshhhh6@gmail.com>
2025-04-28 20:25:43 +0530

    [155] [Time Beats: 62.53%] [Memory Beats: 92.08%] - LeetPush

---
+++ Min-Stack.cpp
@@ -0,0 +1,36 @@
+class MinStack 
+{
+public:
+    vector< pair<int,int> > s;
+\t
+    MinStack() { 
+
+    }
+    
+    void push(int val) 
+    {
+        if(s.empty())
+        {
+            s.push_back({val,val});
+        }    
+        else
+        {
+            s.push_back({val,min(s.back().second,val)});
+        }           
+    }
+    
+    void pop() 
+    { 
+        s.pop_back(); 
+    }
+    
+    int top() 
+    {
+         return s.back().first; 
+    }
+    
+    int getMin() 
+    { 
+        return s.back().second; 
+        }
+};
