f4cfc2636896cb64e38642845bea058ce927d0d8
Varsha <varshhhh6@gmail.com>
2025-04-28 20:28:29 +0530

    [766] [Time Beats: 100.00%] [Memory Beats: 31.70%] - LeetPush

---
+++ Toeplitz-Matrix.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isToeplitzMatrix(vector<vector<int>>& m) 
+    {
+        int r = m.size();
+        int c = m[0].size();
+
+        for (int i = 1; i < r; i++) 
+        {
+            for (int j = 1; j < c; j++) 
+            {
+                if (m[i][j] != m[i - 1][j - 1]) 
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
