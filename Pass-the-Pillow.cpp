aa0ffecb13d4f336cdd474fb9b9f6df3a3401842
Varsha <varshhhh6@gmail.com>
2025-05-11 21:30:11 +0530

    [2582] [Time Beats: 100.00%] [Memory Beats: 11.72%] - LeetPush

---
+++ Pass-the-Pillow.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        int cycle = (n - 1) * 2;
+        time = time % cycle;
+        
+        if (time < n - 1) {
+            return time + 1;
+        } else {
+            return n - (time - (n - 1));
+        }
+    }
+};
