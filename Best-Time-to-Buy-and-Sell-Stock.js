c9c2539a20999d529ba47bbe253565f48ec7de79
Varsha <varshhhh6@gmail.com>
2025-05-13 21:28:23 +0530

    [121] [Time Beats: 41.94%] [Memory Beats: 92.71%] - LeetPush

---
+++ Best-Time-to-Buy-and-Sell-Stock.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let profit = 0;
+    let minPrice = prices[0];
+
+    for (let i = 1; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+        profit = Math.max(profit, prices[i] - minPrice);
+    }
+
+    return profit;
+};
