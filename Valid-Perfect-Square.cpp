addb3c46293f7082bad20b160a5476f76fbf9fdd
Varsha <varshhhh6@gmail.com>
2025-05-14 21:13:05 +0530

    [367] [Time Beats: 100.00%] [Memory Beats: 17.45%] - LeetPush

---
+++ Valid-Perfect-Square.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        int left = 1;
+        int right = num;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long long square = (long long) mid * mid;  
+            if (square == num) {
+                return true;
+            } else if (square < num) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
+    }
+};
