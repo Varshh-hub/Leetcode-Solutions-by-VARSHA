f7c98e4b8857a55b4d2ecfc65dbee95cef68cd56
Varsha <varshhhh6@gmail.com>
2025-05-15 21:43:32 +0530

    [492] [Time Beats: 100.00%] [Memory Beats: 91.86%] - LeetPush

---
+++ Construct-the-Rectangle.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def constructRectangle(self, area):
+        \\\
+        :type area: int
+        :rtype: List[int]
+        \\\
+        width = int(area**0.5)
+        while area % width != 0:
+            width -= 1
+        length = area // width
+        return [length, width]
