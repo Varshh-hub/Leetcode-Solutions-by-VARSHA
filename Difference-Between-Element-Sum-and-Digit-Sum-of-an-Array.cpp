9f6263fb2210f3a8a08582e48d905b579214d781
Varsha <varshhhh6@gmail.com>
2025-06-02 12:11:46 +0530

    [2535] [Time Beats: 100.00%] [Memory Beats: 21.93%] - LeetPush

---
+++ Difference-Between-Element-Sum-and-Digit-Sum-of-an-Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int differenceOfSum(vector<int>& nums) {
+    int sum1 = 0, sum2 = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        sum1 += nums[i];
+        int n = nums[i];
+        while (n > 0) {
+            sum2 += n % 10;
+            n /= 10;
+        }
+    }
+
+    return abs(sum1 - sum2);
+}
+
+};
